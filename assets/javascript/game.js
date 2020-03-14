a60f2c6c311283afb1245a5d1a0d0776110d199a
Ashley Waldner <ashleywaldner22@gmail.com>
2020-03-14 09:42:37 -0500

    add randomNumber to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,27 @@
 var randomAnswer = 0;
 var win = 0;
 var loss = 0;
-var scoreSoFar = 0;
-var crystalOne = ;
-var crystalTwo = ;
-var crystalThree = ;
-var crystalFour = ;
+var crystals;
+var scoreSoFar = [];
+
+// crystals
+$("#pink").on("click", function() {
+    var random = Math.floor(Math.random() * 12)
+    $("#scoreSoFar").text(random);
+    console.log(random)
+})
+
+$("#red").on("click", function() {
+    var random = Math.floor(Math.random() * 12)
+    console.log(random)
+})
+
+$("#grey").on("click", function() {
+    var random = Math.floor(Math.random() * 12)
+    console.log(random)
+})
+
+$("#blue").on("click", function() {
+    var random = Math.floor(Math.random() * 12)
+    console.log(random)
+})
